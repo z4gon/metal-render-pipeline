8182fd3e96f1e19ef30c1615b4be7f106534d945
z4gon <4588601+z4gon@users.noreply.github.com>
2022-11-28 02:31:05 -0300

    Base Project

---
+++ Game Engine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Game Engine
+//
+//  Created by z4gon on 11/28/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
